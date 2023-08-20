92f2e96fa692d04b07ce4c0c131982c9b8bda9ac
mhmadamrii <interceptorghost4@gmail.com>
2023-08-20 19:29:52 +0700

    add something

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({super.key});
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
