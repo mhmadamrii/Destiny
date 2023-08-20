8bd9a22cc9daf5728f6a2ab66893fcaff5898dfa
mhmadamrii <interceptorghost4@gmail.com>
2023-08-20 22:18:04 +0700

    delete something and add something

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cek/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,8 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
-      home: SplashScreen(),
+      title: 'Flutter Demo app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,79 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'package:cek/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    // Future.delayed(const Duration(seconds: 3)).then(
+    //   (value) => {
+    //     Navigator.of(context).pushAndRemoveUntil(
+    //       MaterialPageRoute(
+    //         builder: (context) => const HomeScreen(),
+    //       ),
+    //       (route) => false,
+    //     )
+    //   },
+    // );
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Image.asset('assets/images/background.png'),
+            ),
+          ),
+          Center(
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 22,
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: "Helping you\nto keep ",
+                      style: GoogleFonts.manrope(
+                        fontSize: 24,
+                        color: const Color(0xFFDEE1FE),
+                        letterSpacing: 3.5 / 100,
+                        height: 152 / 100,
+                      ),
+                      children: const [
+                        TextSpan(
+                          text: "your bestie",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        TextSpan(text: "\nstay healthy!")
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
